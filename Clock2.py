bde29e713d042d009ad25cac0745878ded092e6b
draven844 <draven844@gmail.com>
2015-01-16 11:31:47 -0500

    Update 3
    Fix on loops to include resetting alarm

--- Clock2.py
+++ Clock2.py
@@ -12,47 +12,47 @@ from trigger import trigger
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(25, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-
-response = raw_input("Please input the time for the alarm in military format HHMM: \n")
-
-print("Alarm has been set for %s hrs" % response)
-alarm = int(response)
+# You modified this in the morning. Don't forget to push the changes through GIT
 awake = 0
 run = 1
-
 try:
     # Loop to continuously check time, buzz the buzzer for the set alarm time
     while run == 1:
+	#This polls the user for the time in which they want the alarm to go off
+		response = raw_input("Please input the time for the alarm in military format HHMM: \n")
+		print("Alarm has been set for %s hrs" % response)
+		alarm = int(response)
+		
             # Continually gets the time as an integer value
-            curr_time = int(time.strftime("%H%M"))
+		curr_time = int(time.strftime("%H%M"))
 
             # Buzzes the buzzer when the time reaches the set alarm time
-            if curr_time == alarm:
-                    buzz(10,0.5)
-                    time.sleep(0.25)
-                    awake = 1
+		if curr_time == alarm:
+            buzz(10,0.5)
+            time.sleep(0.25)
+            awake = 1
 					
             # Snoozes the alarm for 8 minutes from the current time
             # Only works whilst the alarm is going off
-            if GPIO.input(25) == 1 and awake == 1:
-                    trigger()
-					run = 0
-                    awake = 0
-                    print("Alarm has been silenced. Devices will power on.")
+        if GPIO.input(25) == 1 and awake == 1:
+            trigger()
+			run = 0
+            awake = 0
+            print("Alarm has been silenced. Devices will power on.")
 					
 			# If alarm continues past the set alarm time without being
             # snoozed, the alarm time is changed to the current time.
             # This ensures the alarm buzzes continuously until the
             # snooze button is pressed.
-            elif curr_time != alarm and awake == 1:
-                    alarm = curr_time
-                    buzz(10,0.5)
-					time.sleep(0.25)
+        elif curr_time != alarm and awake == 1:
+            alarm = curr_time
+            buzz(10,0.5)
+		    time.sleep(0.25)
 
 	#This follows the button press. It'll send another signal when activated and go back to the beginning of the following while
-	while run == 0:
-		input("Press enter to send another signal and turn off the relay.")
-		run = 1
+    while run == 0:
+        input("Press enter to send another signal and turn off the relay.")
+	run = 1
 finally:
         GPIO.cleanup()
-		print("End")
+	print("End")
